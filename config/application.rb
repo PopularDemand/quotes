e2c4287be410c6b79e7efbb2996c52189330894c
Alexa <alexa.anderson@outlook.com>
2018-01-07 11:49:21 -0600

    add cors

--- config/application.rb
+++ config/application.rb
@@ -22,9 +22,27 @@ module Quotes
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Load ENV variables if in development
+    # this file must be created locally
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins ENV['ALLOWED_ORIGINS']
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :options]
+      end
+    end
   end
 end
